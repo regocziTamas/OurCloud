8949760d104487bca1978dabc09ede60b9dde3f3
Thomaster <regoczi.tamas01@gmail.com>
2020-01-11 16:03:33 +0100

    Start project.

---
+++ src/main/java/com/thomaster/ourcloud/OurcloudApplication.java
@@ -0,0 +1,13 @@
+package com.thomaster.ourcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomaster/ourcloud/OurcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaster.ourcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
